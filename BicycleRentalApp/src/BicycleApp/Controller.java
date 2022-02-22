b43a6aa039e45a5446f42728484098c784ad6ff3
Madhuri-KS <msalu005@fiu.edu>
2022-02-21 21:31:39 -0500

    Added Few methods

--- BicycleRentalApp/src/BicycleApp/Controller.java
+++ BicycleRentalApp/src/BicycleApp/Controller.java
@@ -7,6 +7,7 @@ public class Controller {
 	public float charging;
 	public Date start_time, end_time;
 	public void calculate_duration(Date start_time) {}
+	
 	public void setStartTime() {}
 	public void setEndTime() {}
 	public void CalculateDistanceTravelled() {}
--- BicycleRentalApp/src/BicycleApp/Customer.java
+++ BicycleRentalApp/src/BicycleApp/Customer.java
@@ -1,9 +1,20 @@
 package BicycleApp;
 
 public class Customer extends Person{
+	
+	public Customer()
+	{
+		Station cust_station = new Station(); 
+		BookingDetails cust_book = new BookingDetails();
+		Cycle cust_cycle=new Cycle();
+		
+		
+		
+	}
 	public static void main(String[] args) {
 		System.out.println("Hey");
 		
+	
 	}
 	
 }
